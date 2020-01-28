20da47fa40f70d6952887f772efdaba32c149407
lucaisacoder <lucas_hsueh@hotmail.com>
2020-01-28 20:57:38 +0800

    build cmake framework
    Change-Id: I3127879d0b9edae62a1459cd96e9e32d820c9181

--- cmake/FindApp.cmake
+++ cmake/FindApp.cmake
@@ -1,3 +1,6 @@
+set(App_ROOT_DIRS ${PROJ_REPO_TOP_DIR}/modules/app)
+#set(App_LIBRARIES libapp.a)
+
 set(_WINDOWS FALSE)
 if("${CMAKE_SYSTEM_NAME}" MATCHES "Windows")
     set(_WINDOWS TRUE)
@@ -10,11 +13,14 @@ endif()
 
 find_path(App_INCLUDE_DIR
     NAMES "app_api.h"
-    PATHS "${PROJ_REPO_TOP_DIR}/modules/app")
-message("${PROJ_REPO_TOP_DIR}/modules/app ${App_INCLUDE_DIR}")
-if(App_INCLUDE_DIR)
+    PATHS "${App_ROOT_DIRS}/*")
+
+find_library(App_LIBRARIES
+    NAMES "libapp.a"
+    HINTS "${App_ROOT_DIRS}/output")
+
+if(App_LIBRARIES)
     set(App_FOUND true)
     set(App_INCLUDE_DIRS ${App_INCLUDE_DIR})
-    #set(App_ROOT_DIRS ${PROJ_REPO_TOP_DIR}/modules/app)
 endif()
 
